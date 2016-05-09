0ba23ecdaf59b429386929265826aaf042d05490
Daniel Bengtsson <daniel@bengtssons.info>
2016-05-10 00:00:55 +0200

    Prevent displaying huge files inline ( ask the user )

---
+++ artifakt/static/js/artifact.js
@@ -0,0 +1,13 @@
+$(function () {
+    $("form.view_content").submit(function (e) {
+        fsize = parseInt($('#file_size').text());
+        if(fsize > 2000000 &&
+            !$(this).hasClass('confirmed')) {
+            e.preventDefault();
+            if (confirm("This file is rather large, are you sure you want to display it ?")) {
+                $(this).addClass('confirmed');
+                $(this).submit();
+            }
+        }
+    });
+});
