feaa5ea55002a6ed3f3b0d8148b180e6ae2c60c4
Daniel Bengtsson <daniel@bengtssons.info>
2016-07-19 19:07:32 +0200

    Added optional firstname lastname and username to register form.

--- artifakt/events.py
+++ artifakt/events.py
@@ -7,4 +7,13 @@ from pyramid_fullauth.events import AfterRegister
 def after_register(event):
     """Redirect to login page after a successful registering"""
     if event.response_values['status']:
+
+        # The standard register view only sets some values
+        # we want a few more
+        post = event.request.POST
+        for data in ['firstname', 'lastname', 'username']:
+            if data in post:
+                setattr(event.user, data, post[data])
+
         raise HTTPFound(location='/login')
+
