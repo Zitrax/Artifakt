1570497e20996da102c5c2cdac0749f20ec080a2
Daniel Bengtsson <daniel@bengtssons.info>
2016-07-24 18:03:33 +0200

    Comments on front page

--- artifakt/views/home.py
+++ artifakt/views/home.py
@@ -2,11 +2,12 @@ from pyramid.view import view_config
 from sqlalchemy import or_
 
 from artifakt import DBSession
-from artifakt.models.models import Artifakt
+from artifakt.models.models import Artifakt, Comment
 
 
 @view_config(route_name='home', renderer='artifakt:templates/home.jinja2')
 def artifact(_):
     return {'artifacts': DBSession.query(Artifakt)
                                   .filter(or_(Artifakt.is_bundle, Artifakt.bundle_id == None))
-                                  .order_by(Artifakt.created.desc()).limit(5)}
+                                  .order_by(Artifakt.created.desc()).limit(5),
+            'comments': DBSession.query(Comment).order_by(Comment.time.desc()).limit(5)}
