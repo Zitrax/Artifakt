42f744f554b8d87f3c97813ab4b44e2171ce5621
Daniel Bengtsson <daniel@bengtssons.info>
2016-10-27 17:24:10 +0200

    Verify synced db/filesystem #7
    Listing zip cache an mails in separate sections.

--- artifakt/views/admin.py
+++ artifakt/views/admin.py
@@ -18,7 +18,7 @@ def admin(_):
 
 
 @view_config(route_name='verify_fs', renderer='artifakt:templates/verify_fs.jinja2', permission='admin')
-def verify_fs(_):
+def verify_fs(request):
     if not storage():
         raise HTTPInternalServerError("storage not set")
     # First check for files in db that are not on disk
@@ -27,11 +27,21 @@ def verify_fs(_):
         if not af.exists:
             af_not_on_disk.append(af)
     # Then check for files on disk that are not in the db
+    maildir = os.path.realpath(request.registry.settings["mail.queue_path"])
+    mails = []
+    zip = []
     af_not_in_db = []
     for root, directories, filenames in os.walk(abspath(storage())):
         for fn in filenames:
             af = DBSession.query(Artifakt).filter(Artifakt.sha1 == root[-2:] + fn).one_or_none()
             if not af:
-                af_not_in_db.append(os.path.join(root, fn))
+                if fn.endswith('.zip'):
+                    zip.append(os.path.join(root, fn))
+                elif root.startswith(maildir):
+                    mails.append(os.path.join(root, fn))
+                else:
+                    af_not_in_db.append(os.path.join(root, fn))
     return {'not_on_disk': af_not_on_disk,
-            'not_in_db': af_not_in_db}
+            'not_in_db': af_not_in_db,
+            'mails': mails,
+            'zip': zip}
