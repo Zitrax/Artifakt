b7f0a11010615f7ca332ac9fc8ac6690c7157aad
Daniel Bengtsson <daniel@bengtssons.info>
2016-11-02 23:26:05 +0100

    Latest deliveries #62

--- artifakt/views/home.py
+++ artifakt/views/home.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config
 from sqlalchemy import or_
 
 from artifakt import DBSession
-from artifakt.models.models import Artifakt, Comment
+from artifakt.models.models import Artifakt, Comment, Delivery
 
 
 @view_config(route_name='home', renderer='artifakt:templates/home.jinja2')
@@ -10,4 +10,6 @@ def artifact(_):
     return {'artifacts': DBSession.query(Artifakt)
                                   .filter(or_(Artifakt.is_bundle, Artifakt.bundles == None))
                                   .order_by(Artifakt.created.desc()).limit(5),
-            'comments': DBSession.query(Comment).filter(~Comment.deleted).order_by(Comment.time.desc()).limit(5)}
+            'comments': DBSession.query(Comment).filter(~Comment.deleted).order_by(Comment.time.desc()).limit(5),
+            'deliveries': DBSession.query(Delivery).order_by(Delivery.time.desc()).limit(5)
+            }
