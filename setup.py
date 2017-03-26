23a8807050b10e961433ffe6860b1b99dbec38b3
Daniel Bengtsson <daniel@bengtssons.info>
2017-03-26 20:20:15 +0200

    Fix version incompatibility for marshmallow-sqlalchemy

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
     'waitress',
     'pyramid_jinja2',
     'pyramid_chameleon',
-    'marshmallow-sqlalchemy',
+    'marshmallow-sqlalchemy <= 0.12.1',  # Seems like we are incompatible with 0.13 now
     'nose',
     'tzf.pyramid_yml',
     'pyramid_fullauth',
